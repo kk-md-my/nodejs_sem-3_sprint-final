436278ca1d355eac7d121f61bacf84b88a3b8233
kostiantyn <kostiantyn@test.com>
2023-12-14 15:22:12 -0330

    +loginController middleware function

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -1,13 +1,29 @@
 // Import external packages
 const express = require("express");
 
+// Import required functions/variables from custom modules
+const { checkCredentials } = require("../controllers/loginController");
+
 // Set router
 const loginRouter = express.Router();
 
 // Set route handlers
-loginRouter.get("/", (req, res) => {
-  res.send("Hey I am Login Page");
-});
+loginRouter
+  .route("/")
+  .get((req, res) => {
+    res.render("login");
+  })
+  .post(checkCredentials, (req, res) => {
+    const { statusCode } = res;
+
+    if (statusCode == 200) {
+      res.redirect("/search");
+    } else if (statusCode == 401) {
+      res.redirect("/");
+    } else if (statusCode == 503) {
+      res.render("503");
+    }
+  });
 
 // Export the router to use in other modules
 module.exports = loginRouter;
