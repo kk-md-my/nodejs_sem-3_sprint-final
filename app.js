cc5631a78e66c3995778d843b2c7f18fe0ce4070
kostiantyn <kostiantyn@test.com>
2023-12-14 12:09:20 -0330

    added back 'loginRouter'

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const methodOverride = require("method-override");
 // Import routes from custom modules
 const indexRouter = require("./routes/indexRouter");
 const signupRouter = require("./routes/signupRouter");
+const loginRouter = require("./routes/loginRouter");
 const searchRouter = require("./routes/searchRouter");
 const resultsRouter = require("./routes/resultsRouter");
 const notFoundRouter = require("./routes/notFoundRouter");
@@ -25,6 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 // Set routes
 app.use("/", indexRouter);
 app.use("/signup", signupRouter);
+app.use("/login", loginRouter);
 app.use("/search", searchRouter);
 app.use("/results", resultsRouter);
 app.use("/*", notFoundRouter);
